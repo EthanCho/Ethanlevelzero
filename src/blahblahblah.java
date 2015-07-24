da7413b80ca414d28dc1e2fce8500ee68c37dc57
EthanCho <ethancho@wts12.attlocal.net>
2015-07-23 20:07:33 -0700

    rollocoaster
    height

---
+++ src/blahblahblah.java
@@ -0,0 +1,42 @@
+import javax.swing.JOptionPane;
+
+
+public class blahblahblah {
+public static void main(String[] args) {
+	String tall=JOptionPane.showInputDialog("how tall are you");
+
+int coaster=Integer.parseInt(tall);
+
+if (coaster>=(4)) {
+	JOptionPane.showMessageDialog(null, "you can go on the ride.");
+}
+if (coaster<(4)) {
+	JOptionPane.showMessageDialog(null, "sorry you can't go on the ride.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
